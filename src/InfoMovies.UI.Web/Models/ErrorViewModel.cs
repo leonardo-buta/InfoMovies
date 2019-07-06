912fa90b567e679cc9e27fef3cb6fd5fd19ddc45
Leonardo Lima <leonardolima9471@gmail.com>
2019-07-06 00:33:33 -0300

    Estrutura do projeto

---
+++ src/InfoMovies.UI.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InfoMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
